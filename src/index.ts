55f23226f9764bb3631fee2e222fdf8148f8cdb6
José Miguel Gonçalves <jose.goncalves@inov.pt>
2023-05-04 09:14:23 +0100

    fix(): Fix release

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,5 @@
-/* ============
- * Keycloak
- * ============
- *
+/*
  * Keycloak comes with a client-side JavaScript library that can be used to secure HTML5/JavaScript applications.
- * The JavaScript adapter has built-in support for Cordova applications.
  *
  * https://www.keycloak.org/docs/latest/securing_apps/#_javascript_adapter
  */
