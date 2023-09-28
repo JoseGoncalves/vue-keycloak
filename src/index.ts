c6acfcc53d969a816b7e0ab55d0190c06f5c3452
José Miguel Gonçalves <jose.goncalves@inov.pt>
2023-09-28 14:12:10 +0100

    feat(): fix release

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,5 @@
 /*
  * Keycloak comes with a client-side JavaScript library that can be used to secure HTML5/JavaScript applications.
- *
  * https://www.keycloak.org/docs/latest/securing_apps/#_javascript_adapter
  */
 
