79eacf20b36e5abf7880a46df2791c0b933c5afd
José Miguel Gonçalves <jose.goncalves@inov.pt>
2024-01-27 22:41:16 +0000

    style(): remove unused eslint-disable-next-line

--- src/utils.ts
+++ src/utils.ts
@@ -1,19 +1,15 @@
-// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types, @typescript-eslint/no-explicit-any
 export function isPromise(promise: any): boolean {
   return !isNil(promise) && typeof promise.then === 'function'
 }
 
-// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types, @typescript-eslint/no-explicit-any
 export function isFunction(fun: any): boolean {
   return !isNil(fun) && typeof fun === 'function'
 }
 
-// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types, @typescript-eslint/no-explicit-any
 export function isString(text: any): boolean {
   return !isNil(text) && (typeof text === 'string' || text instanceof String)
 }
 
-// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types, @typescript-eslint/no-explicit-any
 export function isNil(value: any): boolean {
   return value === undefined || value === null
 }
