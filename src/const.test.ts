51dc3b0ff17ea1f7edd36b542a1ec2675e40c166
José Miguel Gonçalves <jose.goncalves@inov.pt>
2024-07-21 03:09:42 +0100

    test(): fix test description

--- src/const.test.ts
+++ src/const.test.ts
@@ -1,7 +1,7 @@
 import { defaultInitConfig } from './const'
 
 describe('defaultInitConfig', () => {
-  test('should habe the standard baloise config', () => {
+  test('should have the standard config', () => {
     expect(defaultInitConfig.flow).toBe('standard')
     expect(defaultInitConfig.checkLoginIframe).toBe(false)
     expect(defaultInitConfig.onLoad).toBe('login-required')
