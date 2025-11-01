f6c13cf90c8c73fc951d2aea561dcd36a4680f84
José Miguel Gonçalves <jose.goncalves@inov.pt>
2025-11-01 08:48:36 +0000

    chore: remove outdated comment

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,3 @@
-/*
- * Keycloak comes with a client-side JavaScript library that can be used to secure HTML5/JavaScript applications.
- * https://www.keycloak.org/securing-apps/javascript-adapter
- */
-
 export { getToken } from './keycloak'
 export * from './composable'
 export * from './plugin'
